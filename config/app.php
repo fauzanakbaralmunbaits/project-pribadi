007156640013126c8dd285b84c16b2a7273bcf3f
Fauzan Akbar Almunbaits <fauzanakbaralmunbaits@gmail.com>
2018-12-28 17:26:59 +0700

    ubah env and add crudbooster

--- config/app.php
+++ config/app.php
@@ -141,6 +141,7 @@ return [
          * Laravel Framework Service Providers...
          */
         Illuminate\Auth\AuthServiceProvider::class,
+        crocodicstudio\crudbooster\CRUDBoosterServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
